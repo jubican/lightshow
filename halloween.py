b75cc641fd20b6a64bc6b9dd2950f71b5f8049a2
Kevin Cochran <jubican@gmail.com>
2017-10-22 15:04:50 -0700

    update

--- halloween.py
+++ halloween.py
@@ -22,35 +22,57 @@ with open(sys.argv[1], 'r') as f:
 inittime = int(round(time.time()*1000))
 step = 1
 
-pygame.mixer.init()
-pygame.mixer.music.load(sys.argv[2])
-pygame.mixer.music.play()
+main_section = "primary_section"
+currlist = main_section
+seqlist = []
+proclist = []
+
+# pygame.mixer.init()
+# pygame.mixer.music.load(sys.argv[2])
+# pygame.mixer.music.play()
 
 while True:
-  if data[step].startswith("#"):
+  line = data[step].strip().lower()
+  
+  if line.startswith("#") or not line:
     step += 1
     continue
-  current = data[step].split(",")
-  curtime = int(round(time.time()*1000)) - inittime
-  stime = int(current[0]) + 550
-  spin = int(current[1].lstrip())
-  svalue = int(current[2])
-  
-  if stime <= curtime:
-    if spin >= 1 and spin <= 8:
-      print current
+  elif line.startswith("routine_start"):
+    r_temp = line.split()
+    seqlist[r_temp[1]] = []
+    currlist = t_temp[1]
+  elif line.startswith("routine_end"):
+    currlist = main_section
+  elif "," in line:
+    current = line.split(",")
+    if current[0] == "routine" and seqlist[current[1]]:
+      num = int(current[2])
+      for itm in seqlist[current[1]]:
+        seqlist[main_section].append([num+itm[0], itm[1], itm[2]])
+    else:
+      num = int(current[0])
+      lit = int(current[1])
+      cmd = int(current[2])
+      seqlist[currlist].append([num, lit, cmd]])
+    
+    #curtime = int(round(time.time()*1000)) - inittime
+    #stime = int(current[0]) + 550
+    #spin = int(current[1].lstrip())
+    #svalue = int(current[2])
+    
+    #if stime <= curtime:
+    #  if spin >= 1 and spin <= 8:
+    #    print current
       
-      if svalue == 1:
-        GPIO.output(pin[spin-1], GPIO.HIGH)
-      else:
-        GPIO.output(pin[spin-1], GPIO.LOW)
+        # if svalue == 1:
+        #   GPIO.output(pin[spin-1], GPIO.HIGH)
+        # else:
+        #   GPIO.output(pin[spin-1], GPIO.LOW)
+  elif line == "end":
+    #for i in range(len(pin)):
+    #  GPIO.output(pin[i], GPIO.LOW)
+    break
     
-    if spin <= 0:
-      for i in range(len(pin)):
-        GPIO.output(pin[i], GPIO.LOW)
-      break
-    
-    step += 1
+  step += 1
 
-pygame.mixer.music.stop()
-
+# pygame.mixer.music.stop()
