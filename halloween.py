d372eef911702725c8232472c3ed1da1351c467b
Kevin Cochran <jubican@gmail.com>
2017-10-22 19:01:04 -0700

    update

--- halloween.py
+++ halloween.py
@@ -12,7 +12,7 @@ GPIO.setwarnings(False)
 pin = [11,12,13,15,16,18,22,37]
 
 for io in range(len(pin)):
-   GPIO.setup(pin[io], GPIO.OUT)
+  GPIO.setup(pin[io], GPIO.OUT)
 
 with open(sys.argv[1], 'r') as f:
   data = f.readlines()
@@ -20,7 +20,7 @@ with open(sys.argv[1], 'r') as f:
     data[i] = data[i].rstrip()
 
 inittime = int(round(time.time()*1000))
-step = 1
+step = 0
 
 main_section = "primary_section"
 sections = []
@@ -28,33 +28,27 @@ sections.append(main_section)
 currlist = sections.index(main_section)
 seqlist = []
 proclist = []
-
-print "begin: " + str(currlist)
-# pygame.mixer.init()
-# pygame.mixer.music.load(sys.argv[2])
-# pygame.mixer.music.play()
+seqlist.append([])
 
 while True:
   line = data[step].strip().lower()
-  
+    
   if line.startswith("#") or not line:
     step += 1
     continue
   elif line.startswith("routine_start"):
     r_temp = line.split()
-    sections.append(r_temp[1])
+    sections.append(r_temp[1].strip())
     currlist = sections.index(r_temp[1])
-    seqlist[currlist] = []
+    seqlist.append([])
   elif line.startswith("routine_end"):
     currlist = sections.index(main_section)
-    print "end:" + str(currlist)
   elif "," in line:
-    print "line:" + str(currlist)
     current = line.split(",")
-    if current[0] == "routine" and sections.index(current[1]):
+    if current[0] == "routine" and sections.index(current[1].strip()):
       num = int(current[2])
-      secitm = sections.index(current[1])
-      secmain = sections.index(main_section)
+      secitm = sections.index(current[1].strip())
+      secmain = sections.index(main_section.strip())
       for itm in seqlist[secitm]:
         seqlist[secmain].append([num+itm[0], itm[1], itm[2]])
     else:
@@ -62,25 +56,34 @@ while True:
       lit = int(current[1])
       cmd = int(current[2])
       seqlist[currlist].append([num, lit, cmd])
-    
-    #curtime = int(round(time.time()*1000)) - inittime
-    #stime = int(current[0]) + 550
-    #spin = int(current[1].lstrip())
-    #svalue = int(current[2])
-    
-    #if stime <= curtime:
-    #  if spin >= 1 and spin <= 8:
-    #    print current
-      
-        # if svalue == 1:
-        #   GPIO.output(pin[spin-1], GPIO.HIGH)
-        # else:
-        #   GPIO.output(pin[spin-1], GPIO.LOW)
   elif line == "end":
-    #for i in range(len(pin)):
-    #  GPIO.output(pin[i], GPIO.LOW)
+    for i in range(len(pin)):
+      GPIO.output(pin[i], GPIO.LOW)
     break
     
   step += 1
 
-# pygame.mixer.music.stop()
+pygame.mixer.init()
+pygame.mixer.music.load(sys.argv[2])
+pygame.mixer.music.play()
+
+step = 0
+last = len(seqlist[0])
+while step < last:
+  curtime = int(round(time.time()*1000)) - inittime
+  stime = int(i[0]) + 550
+  spin = int(i[1].lstrip())
+  svalue = int(i[2])
+  
+  if stime <= curtime:
+    if svalue == 1:
+      GPIO.output(pin[spin-1], GPIO.HIGH)
+    else:
+      GPIO.output(pin[spin-1], GPIO.LOW)
+  print(i)
+  step += 1
+
+for io in range(len(pin)):
+  GPIO.output(pin[spin-1], GPIO.LOW)
+
+pygame.mixer.music.stop()
